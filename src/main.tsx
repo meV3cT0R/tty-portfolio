6245956d86843832944c9516fe29b3bea47d868f
MeV3cT0R <shresthasumit09876@gmail.com>
2024-08-18 17:34:08 +0545

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import AppProvider from './context/AppContext.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
-
       <App />
+      <Analytics/>
     </AppProvider>
   </StrictMode>,
 )
